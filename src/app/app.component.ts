30bc1d8b170d5cee78255b294e7da2b0941213b9
patrickfcarey <patrickfcarey@gmail.com>
2025-04-12 14:33:30 -0500

    reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,CommonModule,FormsModule],
+  imports: [RouterOutlet,CommonModule,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-
+export class AppComponent implements OnInit{
+  myForm: FormGroup = new FormGroup({});
   
-
-   submitForm(){
-     console.log("form submitted");
-   }
+  constructor(private formBuilder: FormBuilder){
+    ngOnInit(): void{
+      this.myForm = this.formBuilder.group({
+        name:['', Validators.required],
+        email:['', [Validators.required,Validators.email]]
+      })
+    }
+  }
 }
