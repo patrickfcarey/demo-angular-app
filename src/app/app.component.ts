2fb05aca7c5626ffbfab198e4c759e4eb9b6d68f
patrickfcarey <patrickfcarey@gmail.com>
2025-04-12 14:09:50 -0500

    template driven form example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,CommonModule],
+  imports: [RouterOutlet,CommonModule,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-   value: string = "Hello Angular";
-   currentDate: Date = new Date();
-   amount: number = 99.99;
+
   
+
+   submitForm(){
+     console.log("form submitted");
+   }
 }
